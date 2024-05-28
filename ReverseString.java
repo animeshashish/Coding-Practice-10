df2c779c76b6e89e444cc97b9bc6e48290f99964
Animesh Hemrom <animeshashishhemrom@gmail.com>
2024-05-28 15:47:58 +0530

    correction made

--- ReverseString.java
+++ ReverseString.java
@@ -14,11 +14,9 @@ public class ReverseString {
         for (int i = str.length() -1; i >= 0; i--) {
             reverseStr = reverseStr + str.charAt(i);
 
-            System.out.println("Reversed string is:" + reverseStr);
-
         }
 
-
+        System.out.println("Reversed string is:" + reverseStr);
 
     }
 }
